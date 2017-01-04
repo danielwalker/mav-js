7a19ca7c8b3e2f4b472e33b16375e84184d25c80
danielwalker <daniel.walker@structure6.com>
2017-01-04 13:54:01 +1100

    flyto

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,13 @@ var _ = require('underscore');
 
 let mavlinkParser = new MAVLink(winston,254,0);
 
-let connection = net.createConnection(5762, '192.168.1.206', function() {
-    streamAll();    
-    setGuided();
-    arm();
-    takeOff();
+//let connection = net.createConnection(5763, '10.11.2.190', function() {
+let connection = net.createConnection(5760, '127.0.0.1', function() {
+    //streamAll();    
+    //setGuided();
+    //arm();
+    //takeOff();
+    flyto();
 });
 
 connection.on('data', function(data) {    
@@ -25,12 +27,22 @@ mavlinkParser.on('HEARTBEAT', function(message) {
     console.log('RX HEARTBEAT');
 });
 
-mavlinkParser.on('COMMAND_ACK', function(message) {    
+mavlinkParser.on('SYS_STATUS', function(message) {    
+        //console.log(message);
+});
+
+mavlinkParser.on('STATUSTEXT', function(message) {    
+        console.log(message.text);
+});
+
+mavlinkParser.on('COMMAND_ACK', function(message) {        
     console.log('RX ACK', message.command + ' = ' + message.result + ' (' + ( message.result == 0 ? 'OK' : 'FAILED') + ')') ;
 });
 
 mavlinkParser.on('GLOBAL_POSITION_INT', function(message) {    
     console.log('ALT (m)', message.relative_alt/1000);
+    console.log('LAT', message.lat);    
+    console.log('LNG', message.lon);            
 });
 
 function setGuided() {
@@ -58,6 +70,25 @@ function streamAll() {
     message = new mavlink.messages.request_data_stream(1, 1, mavlink.MAV_DATA_STREAM_ALL, 1, 1);
     buffer = new Buffer(message.pack(mavlinkParser));
     connection.write(buffer);
+}
+
+
+function flyto() {
+   console.log('TRYING TO FLY TO');
+   message = new mavlink.messages.mission_item(
+       1, 
+       0, 
+       0, 
+       mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,            
+       mavlink.MAV_CMD_NAV_WAYPOINT, 
+       0, 
+       0,
+       0,0,0,0
+       -35.363405, 149.168045, 10);
+
+   buffer = new Buffer(message.pack(mavlinkParser));
+   connection.write(buffer)
+   console.log('FLYTO');
 }
 
 
