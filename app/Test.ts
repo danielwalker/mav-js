2848127a9f69883516bfbb617328c495574abd21
danielwalker <daniel.walker@structure6.com>
2017-01-11 23:56:15 +1100

    refactor

--- app/Test.ts
+++ app/Test.ts
@@ -6,8 +6,12 @@ let mav: Mav = new Mav(5760, '127.0.0.1');
 mav.connect();
 
 mav.on('status', (status: Status) => {
+
+    // MAV in standby (ready to launch)
     if (status == Status.STANDBY) {
         log.debug('ready to fly');
+
+        // Set guided mode.
         mav.setGuided(function() {
             log.debug('successfully set guided mode');
         }, function(err) {
--- dist/app/Test.js
+++ dist/app/Test.js
@@ -4,8 +4,10 @@ const Log_1 = require("./Log");
 let mav = new Mav_1.Mav(5760, '127.0.0.1');
 mav.connect();
 mav.on('status', (status) => {
+    // MAV in standby (ready to launch)
     if (status == Mav_1.Status.STANDBY) {
         Log_1.log.debug('ready to fly');
+        // Set guided mode.
         mav.setGuided(function () {
             Log_1.log.debug('successfully set guided mode');
         }, function (err) {
